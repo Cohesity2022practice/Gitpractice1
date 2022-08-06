55d4166eb43d8bd44d6ab87d6ca09d3dfdb8ac44
karchikiran6 <kiran.karchi@GINIMINDS.LOCAL>
2022-08-06 12:28:26 +0530

    added in test env

--- Test1.sh
+++ Test1.sh
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 adding extra line 
 dding line to merge with different branch master 
+=======
+updating in  dev envirnmnent 
+>>>>>>> 1a53624 (updated in Test Envi)
 this my first line of program
 updated from GUI 
