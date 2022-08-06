14b190d6994f80a8524516737aa43790e5c009b2
karchikiran6 <kiran.karchi@GINIMINDS.LOCAL>
2022-08-06 12:29:37 +0530

    updated ......updated

--- Test1.sh
+++ Test1.sh
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 adding extra line 
 dding line to merge with different branch master 
 =======
 updating in  dev envirnmnent 
 >>>>>>> 1a53624 (updated in Test Envi)
+=======
+updating in  Test envirnmnent 
+>>>>>>> 286870a (commited in Test branch)
 this my first line of program
 updated from GUI 
